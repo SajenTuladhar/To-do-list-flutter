517faa1707581f2038d3b18c45133c610095b60a
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-03-16 21:35:34 +0545

    add check boxes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,20 +12,31 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 38, 38, 38),
-      appBar: AppBar(
-        title: const Text("To Do List"),
-        titleTextStyle: const TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 19, 19, 19),
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(50))),
-        leading: const Icon(Icons.hourglass_empty),
-        foregroundColor: Colors.white,
-      
-      ),
-    );
+        backgroundColor: const Color.fromARGB(255, 38, 38, 38),
+        appBar: AppBar(
+          title: const Text("To Do's"),
+          titleTextStyle: const TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255), fontSize: 21),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(50))),
+        ),
+        body: ListView(
+          children:  [
+            ToDoTile(taskName:"Power ON", 
+            taskCompeleted: true ,
+            onChanged: (p0) {
+              
+            },),
+            ToDoTile(taskName:"Power off", 
+            taskCompeleted: false ,
+            onChanged: (p0) {
+              
+            },),
+                     ],
+        ));
   }
 }
--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  final String taskName;
+  final bool taskCompeleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile({super.key,required this.taskName,required this.taskCompeleted, required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, top: 40),
+      child: Container(
+        padding: const EdgeInsets.all(26),
+        // ignore: prefer_const_constructors
+        decoration: BoxDecoration(
+            color:const Color.fromARGB(255, 93, 92, 92),
+            borderRadius:
+              const BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(0),topLeft: Radius.circular(20))),
+        child: Row(
+          children: [
+            //chekcbox
+          Checkbox(value: taskCompeleted, onChanged: onChanged),
+
+            //tasks
+             Text(taskName),
+          ],
+        ),
+        
+      ),
+    );
   }
 }
