71012e9282e38608603b41e77b92349fcde897b7
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-03-16 17:54:44 +0545

    home page and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/pages/first_page.dart';
 import 'package:todo_list/pages/home_page.dart';
-import 'package:todo_list/pages/settings_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   MaterialApp(
+    return  const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  FirstPage(),
-      routes: {
-        '/firstpage':(context) =>  FirstPage(),
-        '/homepage':(context)=> const HomePage(),
-        '/settingspage':(context)=> const SettingsPage()
-      },
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,30 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      backgroundColor: Colors.pink[200],
-      body:Center(child:  Text("Home page"),)
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 38, 38, 38),
+      appBar: AppBar(
+        title: const Text("To Do List"),
+        titleTextStyle: const TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(50))),
+        leading: const Icon(Icons.hourglass_empty),
+        foregroundColor: Colors.white,
+      
+      ),
     );
   }
 }
